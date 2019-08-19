53b42c9a97d8cefcfe5651edf5190e113f6dab12
Evgeny Lepet <sadjem1990@gmail.com>
2019-08-19 20:05:30 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+package HomeWork141;
+
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) {
+        Test test = new Test();
+        try {
+            testReflection(test, "test2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void testReflection(Test test, String test2) throws Exception{
+            Method  method = test.getClass().getMethod("test2");
+            method.invoke(test);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,15 @@
+package HomeWork141;
+
+public class Test {
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    public void test2() {
+        System.out.println("test2");
+    }
+
+    public void test3() {
+        System.out.println("test3");
+    }
+}
